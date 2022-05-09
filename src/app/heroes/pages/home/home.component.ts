ba655b7443169d6eff2389f3a8b10aa416bab15d
Facundo Arce <40859125+facundo876@users.noreply.github.com>
2022-05-09 00:11:54 -0300

    login service

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { HeroesService } from '../../services/heroes.service';
 import { Heroes } from '../../interfaces/heroes.interfaces';
+
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -12,14 +16,19 @@ import { Heroes } from '../../interfaces/heroes.interfaces';
   `
   ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
 
-  constructor() { }
+  get auth(){
+    return this.authService.Auth;
+  }
 
-  ngOnInit(): void {
+  constructor( private router:Router,
+               private authService: AuthService ) { }
 
 
+  logout(){
+    this.router.navigate(['./auth'])
   }
 
 }
